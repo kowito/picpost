c676c1a900e2647776338d0ebd9bd500ad659a26
Kowit Charoenratchatabhan <kowit.s.c@gmail.com>
2018-08-28 15:58:21 +0700

    upgrade to django 2

--- picpost/urls.py
+++ picpost/urls.py
@@ -1,19 +1,19 @@
-from django.conf.urls import patterns, include, url
+from django.urls import include, path, re_path
+
 
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    
+urlpatterns = [
     # url(r'^$', 'picpost.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^gallery/', include('imagestore.urls', namespace='imagestore')),
-    url(r'^admin/', include(admin.site.urls)),
-)
+    re_path(r'^gallery/', include('imagestore.urls', namespace='imagestore')),
+    re_path(r'^admin/', include(admin.site.urls)),
+]
 
 import settings
 if settings.DEBUG:
     import debug_toolbar
     urlpatterns += patterns('',
-        url(r'^__debug__/', include(debug_toolbar.urls)),
-        )
+                            url(r'^__debug__/', include(debug_toolbar.urls)),
+                            )
